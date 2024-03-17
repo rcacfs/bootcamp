a7d8fe4ec840df036de832399308fe12ade39b40
marcrine-geek <marcrinemm@gmail.com>
2024-03-17 08:57:15 +0300

    route table updates

--- route_tables.tf
+++ route_tables.tf
@@ -1,3 +1,4 @@
+// public subnet route table
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.vpc.id
 
@@ -16,26 +17,7 @@ resource "aws_route_table_association" "public-subnet_a" {
   route_table_id = aws_route_table.public-route-table.id
 }
 
-
-resource "aws_route_table" "elb-web-public-route-table" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway.id
-  }
-
-  tags = {
-    Name = "bootcamp-elb-web-public-route-table"
-  }
-}
-
-resource "aws_route_table_association" "elb-web-public-subnet-a" {
-  subnet_id      = aws_subnet.elb-web-public-a.id
-  route_table_id = aws_route_table.elb-web-public-route-table.id
-}
-
-
+// web subnet route table association
 resource "aws_route_table" "web-private-route-table" {
   vpc_id = aws_vpc.vpc.id
 
@@ -54,6 +36,36 @@ resource "aws_route_table_association" "web-private-subnet-a" {
   route_table_id = aws_route_table.web-private-route-table.id
 }
 
+resource "aws_route_table_association" "web-private-subnet-b" {
+  subnet_id      = aws_subnet.web-private-b.id
+  route_table_id = aws_route_table.web-private-route-table.id
+}
+
+// app subnet route table association
+resource "aws_route_table" "app-private-route-table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_a.id
+  }
+
+  tags = {
+    Name = "bootcamp-app-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "app-private-subnet-a" {
+  subnet_id      = aws_subnet.app-private-a.id
+  route_table_id = aws_route_table.app-private-route-table.id
+}
+
+resource "aws_route_table_association" "app-private-subnet-b" {
+  subnet_id      = aws_subnet.app-private-b.id
+  route_table_id = aws_route_table.app-private-route-table.id
+}
+
+// database subnet association
 resource "aws_route_table" "db-private-route-table" {
   vpc_id = aws_vpc.vpc.id
 
@@ -66,7 +78,12 @@ resource "aws_route_table_association" "db-private-subnet-a" {
   subnet_id      = aws_subnet.db-private-a.id
   route_table_id = aws_route_table.db-private-route-table.id
 }
+resource "aws_route_table_association" "db-private-subnet-b" {
+  subnet_id      = aws_subnet.db-private-b.id
+  route_table_id = aws_route_table.db-private-route-table.id
+}
 
+// bastion subnet association
 resource "aws_route_table" "bastion-public-route-table" {
   vpc_id = aws_vpc.vpc.id
 
